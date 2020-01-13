67baf988308d311dc3ddf771108bd042384e98b6
Robert0816 <duschek@gmx.net>
2020-01-13 16:50:06 +0100

    Spring & JDBC 1 erledigt

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.sql.*;
 
 @Controller
 public class WizardController {
+
+       private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
+	     private final static String DB_USER = "h4rryp0tt3r";
+	     private final static String DB_PASSWORD = "Horcrux4life!";
 
     private WizardRepository repository = new WizardRepository();
 
